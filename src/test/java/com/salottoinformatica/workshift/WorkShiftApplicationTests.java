15ec88eec0d7a608e5ca81c423467760a7a36456
eD <ed.pierri@outlook.com>
2023-04-29 16:11:53 +0200

    Commit iniziale progetto

---
+++ src/test/java/com/salottoinformatica/workshift/WorkShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salottoinformatica.workshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
